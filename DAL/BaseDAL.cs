436df8d261bcfd948c71167241e1ed3727ee39d1
Sunil3210 <ssj55322@gmail.com>
2025-08-12 19:53:44 +0530

    DepartmentDAL implementation Changes

---
+++ DAL/BaseDAL.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace DAL
+{
+    public class BaseDAL
+    {
+        private string connectionstring;
+        public BaseDAL(IConfiguration configuration)
+        {
+            this.connectionstring = configuration.GetConnectionString("HospitalManagement");
+        }
+        //PassConnection string and it will create Connection to the Database
+        public SqlConnection CreateConnection()
+        {
+            return new SqlConnection(connectionstring);
+        }
+    }
+}
---
+++ DAL/DepartmentDAL.cs
@@ -0,0 +1,172 @@
+﻿using DAL.Entity;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public interface IDepartmentDAL
+    {
+        Task<int> Create(Department department);
+        Task<int> Update(Department department);
+        Task<Department> GetById(int id);
+    }
+    public class DepartmentDAL : BaseDAL, IDepartmentDAL
+    {
+        public DepartmentDAL(IConfiguration configuration) : base(configuration) { }
+
+        #region Create
+
+        /// <summary>
+        /// Create Department
+        /// </summary>
+        /// <param name="department"></param>
+        /// <returns></returns>
+        public async Task<int> Create(Department department)
+        {
+            int newId = 0;
+            var parms = new SqlParameter[]
+                    {
+                        new SqlParameter(){
+                            ParameterName ="@Name",
+                            SqlDbType = SqlDbType.NVarChar,
+                            IsNullable=true,
+                            Value = department.Name,
+                            Direction = ParameterDirection.Input,
+                        },
+                        new SqlParameter(){
+                            ParameterName ="@Id",
+                            SqlDbType = SqlDbType.NVarChar,
+                            IsNullable=true,
+                            Value = department.Id>0?department.Id:null,
+                            Direction = ParameterDirection.Input,
+                        }
+                    };
+            var outPutParameter = new SqlParameter()
+            {
+                ParameterName = "@NewId",
+                SqlDbType = SqlDbType.Int,
+                Direction = ParameterDirection.Output,
+            };
+
+            using (var connection = CreateConnection())
+            {
+                using (var command = connection.CreateCommand())
+                {
+                    connection.Open();
+                    command.CommandText = "sp_Department_CreateOrUpdate";
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddRange(parms);
+                    command.Parameters.Add(outPutParameter);
+                    command.Connection = connection;
+                    await command.ExecuteNonQueryAsync();
+                }
+            }
+            return newId;
+
+        }
+
+        #endregion
+
+        #region Update
+
+        /// <summary>
+        /// Update Department
+        /// </summary>
+        /// <param name="department"></param>
+        /// <returns></returns>
+        public async Task<int> Update(Department department)
+        {
+            int retVal=-1;
+            var parms = new SqlParameter[]
+                    {
+                        new SqlParameter(){
+                            ParameterName ="@Name",
+                            SqlDbType = SqlDbType.NVarChar,
+                            IsNullable=true,
+                            Value = department.Name,
+                            Direction = ParameterDirection.Input,
+                        },
+                        new SqlParameter(){
+                            ParameterName ="@Id",
+                            SqlDbType = SqlDbType.NVarChar,
+                            IsNullable=true,
+                            Value = department.Id>0?department.Id:null,
+                            Direction = ParameterDirection.Input,
+                        }
+                    };
+            
+            var returnParameter = new SqlParameter()
+            {
+                ParameterName = "@ReturnVal",
+                SqlDbType = SqlDbType.Int,
+                Direction = ParameterDirection.ReturnValue,
+            };
+            using (var connection = CreateConnection())
+            {
+                using (var command = connection.CreateCommand())
+                {
+                    connection.Open();
+                    command.CommandText = "sp_Department_CreateOrUpdate";
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddRange(parms);
+                    command.Parameters.Add(returnParameter);
+                    command.Connection = connection;
+                    await command.ExecuteNonQueryAsync();
+                }
+            }
+            return retVal;
+
+        }
+
+        #endregion
+
+        #region GetById
+
+        /// <summary>
+        /// Get Department By Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<Department> GetById(int id)
+        {
+            Department department = null;
+
+            using (var connection = CreateConnection())
+            {
+                using (var command = connection.CreateCommand())
+                {
+                    connection.Open();
+                    command.CommandText = "sp_Department_GetById";
+                    command.Parameters.Add(new SqlParameter()
+                    {
+                        ParameterName = "@Id",
+                        SqlDbType = SqlDbType.NVarChar,
+                        Direction = ParameterDirection.Input,
+                        Value = id,
+                        IsNullable = false
+                    });
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Connection = connection;
+                    var dataReader = await command.ExecuteReaderAsync();
+                    while (dataReader.Read())
+                    {
+                        department = new Department()
+                        {
+                            Id = Convert.ToInt32(dataReader["Id"]),
+                            Name = dataReader["Name"].ToString(),
+                        };
+                    }
+                }
+            }
+            return department;
+        }
+
+        #endregion
+    }
+}
